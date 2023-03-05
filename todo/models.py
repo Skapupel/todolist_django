f314203dd32d7294ea872dd8fda1633ac4e3503f
Skapupel <bkd.slava@gmail.com>
2023-03-05 19:42:36 +0200

    New todo model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
+from users.models import User
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
