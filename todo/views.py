7e96180e84750e687ebbbfabb9e6f7e15a1192e3
Skapupel <bkd.slava@gmail.com>
2023-03-05 20:19:16 +0200

    Add Todo app views

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Todo
+from .serializers import TodoSerializer
 
-# Create your views here.
+
+class TodoListCreateAPIView(generics.ListCreateAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return Todo.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class TodoDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return Todo.objects.filter(user=self.request.user)
